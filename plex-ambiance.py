2fc6cdf9b3365aca1b4b6846086ce72e5aad1afc
Vincent Bedard <vedard@gmail.com>
2021-12-10 03:30:39 +0000

    Add plex token + fix a bug

--- plex-ambiance.py
+++ plex-ambiance.py
@@ -10,23 +10,31 @@ from lxml import etree
 @click.command()
 @click.option("--plex-server",  required=True, envvar='PLEX_SERVER', help='The URI for the Plex Media Server')
 @click.option("--plex-client", required=True, envvar='PLEX_CLIENT', help='The name of the Plex client')
+@click.option("--plex-token", default="", envvar='PLEX_TOKEN', help='The plex API token')
 @click.option("--hue-bridge", required=True, envvar='HUE_BRIDGE', help='The URI of the Hue Bridge')
 @click.option("--hue-token", required=True, envvar='HUE_TOKEN', help='The Hue API token')
 @click.option("--on", default=['0'], multiple=True, envvar='GROUPS_ON', help="The group of lights you want to turn on")
 @click.option("--off", default=['0'], multiple=True, envvar='GROUPS_OFF', help="The group of lights you want to turn on")
-def main(plex_server, plex_client, hue_bridge, hue_token, on, off):
+def main(plex_server, plex_client, plex_token, hue_bridge, hue_token, on, off):
     """ Synchronize your Philips Hue lightbulbs with your Plex playback
     """
 
     last_state = None
+    
     click.echo(f"Plex server: {plex_server}")
     click.echo(f"Hue bridge: {hue_bridge}")
     click.echo(f"Lights to turn on: {', '.join(on)}")
     click.echo(f"Lights to turn off: {', '.join(off)}")
     click.echo(f"Listening for this device: {plex_client}")
+
+
+    plex_url = f"{plex_server}/status/sessions"
+    if plex_token:
+        plex_url += f"?X-Plex-Token={plex_token}"
+
     while True:
         try:
-            response = etree.parse(f"{plex_server}/status/sessions")
+            response = etree.parse(plex_url)
             new_state = response.xpath(f'string(/MediaContainer/Video/Player[@device="{plex_client}"]/@state)')
 
             # Fallback unknown value to stopped
@@ -49,8 +57,8 @@ def main(plex_server, plex_client, hue_bridge, hue_token, on, off):
                 for group in off:
                     requests.put(f"{hue_bridge}/api/{hue_token}/groups/{group}/action", json={"on": False})
 
-            # Turn on the lights when a media is stopped or paused
-            else:
+            # Turn on the lights when a media is stopped or paused (except the first time we start the service)
+            elif last_state != None:
                 click.echo("Turning the lights on")
                 for group in on:
                     requests.put(f"{hue_bridge}/api/{hue_token}/groups/{group}/action", json={"on": True})
